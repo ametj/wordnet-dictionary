7860a09ac20f0692a7aceab86041d51e742e7a87
Matej Leško <lesko.matej@outlook.com>
2021-05-11 14:01:01 +0200

    Make lexical entry list responsive

---
+++ WordNet.Wpf/Converters/IsLastItemConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace WordNet.Wpf.Converters
+{
+    public class IsLastItemConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var item = (DependencyObject)value;
+            var ic = ItemsControl.ItemsControlFromItemContainer(item);
+
+            return ic.ItemContainerGenerator.IndexFromContainer(item) == ic.Items.Count - 1;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
